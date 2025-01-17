1d59717c512c49c078430ed7f7b88781a94f3ffa
zhangyixin <2372944924@qq.com>
2025-01-17 16:01:07 +0800

    添加base根目录

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import sidebar from './sidebar.mjs'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/vitePress/',
   title: "My Project",
   description: "A VitePress Site",
   outDir: "dist", //输出目录
